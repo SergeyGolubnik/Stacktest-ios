d658f230447d2467a1700f775382baf38dcf2497
Sergey <sgolubnik@gmail.com>
2022-06-14 22:32:57 +0300

    Initial Commit

---
+++ Stacktest-ios/Stacktest_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stacktest_iosApp.swift
+//  Stacktest-ios
+//
+//  Created by СОВА on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Stacktest_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
